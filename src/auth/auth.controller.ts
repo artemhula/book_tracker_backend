0d035c8d46459ec9554810580257baa599c6d2fb
artem <artemgul4@gmail.com>
2025-09-17 17:14:47 +0300

    Enhanced refresh token handling

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
+import express from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -12,19 +13,40 @@ export class AuthController {
 
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
-  async googleAuthRedirect(@Req() req) {
+  async googleAuthRedirect(@Req() req, @Res() res: express.Response) {
     const user = await this.authService.findUserOrCreate({
       email: req.user.email,
       fullName: req.user.name,
       avatarUrl: req.user.avatarUrl,
     });
 
-    return this.authService.generateTokensAndSave(user);
+    const { accessToken, refreshToken } =
+      await this.authService.generateTokensAndSave(user);
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+    });
+
+    res.redirect(
+      `${process.env.FRONTEND_URL_CALLBACK}?accessToken=${accessToken}`
+    );
   }
 
   @Post('refresh')
-  async refreshAccessToken(@Body('refreshToken') refreshToken: string) {
-    const accessToken = await this.authService.refreshAccessToken(refreshToken);
-    return accessToken;
+  async refreshAccessToken(@Req() req: express.Request) {
+    const refreshToken = req.cookies['refreshToken'];
+    const { accessToken } =
+      await this.authService.refreshAccessToken(refreshToken);
+
+    return { accessToken };
+  }
+
+  @Post('logout')
+  logout(@Res() res: express.Response) {
+    res.clearCookie('refreshToken');
+    return { ok: true };
   }
 }
